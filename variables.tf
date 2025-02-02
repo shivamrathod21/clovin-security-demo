2f01757a35190b12e4d864c4d60158b473f26e9c
shivam721 <rathodshivam832@gmail.com>
2025-02-02 20:56:35 +0530

    feat: Re-add Docker build and push to workflow

--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
+# Docker Hub username
 variable "docker_username" {
   description = "Docker Hub username"
   type        = string
 }
 
+# Database password
 variable "db_password" {
   description = "Password for the RDS instance"
   type        = string
   sensitive   = true
 }
 
+# SSH key path
 variable "public_key_path" {
   description = "Path to the public key file for EC2 SSH access"
   type        = string
-  default     = "~/.ssh/id_rsa.pub"  # Default path to public key
+  default     = "~/.ssh/id_rsa.pub"
 }
